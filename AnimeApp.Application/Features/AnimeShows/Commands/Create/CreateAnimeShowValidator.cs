2630a16da683ac6d082e2f01a74032857e3900d5
lenovo <ahmedkhaledibrahim20@gmail.com>
2025-02-17 22:24:29 +0200

    fluent validation implementation for create anime show command

---
+++ AnimeApp.Application/Features/AnimeShows/Commands/Create/CreateAnimeShowValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeApp.Application.Features.AnimeShows.Commands.Create
+{
+    public class CreateAnimeShowValidator : AbstractValidator<CreateAnimeShowCommand>
+    {
+        public CreateAnimeShowValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(100);
+
+            RuleFor(x => x.Description).NotEmpty().MaximumLength(500);
+        }
+    }
+}
--- AnimeApp/Program.cs
+++ AnimeApp/Program.cs
@@ -10,6 +10,15 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
